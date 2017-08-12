ae6472b60774562894d74cedf08cdc4439a012cb
Oriol Farrús Lacasa <dasyrch@gmail.com>
2017-08-12 19:43:55 +0100

    Edit IntervalMaster name

--- app/controllers/interval_master_controller.rb
+++ app/controllers/interval_master_controller.rb
@@ -4,8 +4,20 @@ class IntervalMasterController < ApplicationController
     @interval_master = IntervalMaster.find(params[:id])
   end
 
+  def update
+    @interval_master = IntervalMaster.find(params[:id])
+    @interval_master.update_attributes(interval_master_params)
+
+    redirect_to @interval_master
+  end
+
   def show
     @interval_master = IntervalMaster.find(params[:id])
   end
 
+  private
+
+      def interval_master_params
+        params.require(:interval_master).permit(:name)
+      end
 end
